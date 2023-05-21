ae008c11515a662151354afef22cfeeb1fb374bf
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-05-21 14:51:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+//Initial code to create our server
+const http = require("http"),
+  path = require("path"),
+  express = require("express"),
+  bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+app.use(express.static("."));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Create sqlite database and populate it
+const db = new sqlite3.Database(":memory:");
+db.serialize(function () {
+  db.run(`CREATE TABLE user (username TEXT, password TEXT, title TEXT)`);
+  db.run(
+    `INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')
+`
+  );
+});
+
+//Home page
+app.get("/", (req, res) => {
+  res.sendFile(`index.html`);
+});
+
+//Display and handle login
+app.post("/login", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+  
+    let query =
+      "SELECT title FROM user where username = '" +
+      username +
+      "' and password = '" +
+      password +
+      "'";
+  
+    console.log(username, password, query);
+  
+    db.get(query, function (err, row) {
+      if (err) {
+        console.log("ERROR", err);
+        res.redirect("/index.html#error");
+      } else if (!row) {
+        res.redirect("/index.html#unauthorized");
+      } else {
+        res.send(`Hello <b>' + ${row.title} + '!</b><br /> 
+              This file contains all your secret data: <br /><br /> 
+              SECRETS <br /><br /> MORE SECRETS <br /><br /> 
+              <a href='/index.html'>Go back to login</a>`);
+      }
+    });
+  });
+
+app.listen(3006);
